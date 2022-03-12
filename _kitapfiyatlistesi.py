589278c9d5819185533dfb4a85764d6fcbd02925
selimcanermis <ermisselimcan@gmail.com>
2022-03-13 01:39:57 +0300

    Add Idefix and author check

--- _kitapfiyatlistesi.py
+++ _kitapfiyatlistesi.py
@@ -1,230 +1,151 @@
 import requests
 from bs4 import BeautifulSoup
 
-ithaki_url = "https://www.dr.com.tr/Yayinevi/ithaki-yayinlari/s=5119"
-isbankasi_url = "https://www.dr.com.tr/Yayinevi/is-bankasi-kultur-yayinlari/s=5109"
-yky_url = "https://www.dr.com.tr/Yayinevi/yapi-kredi-yayinlari/s=10615"
-can_url = "https://www.dr.com.tr/Yayinevi/can-yayinlari/s=1883"
-dogan_url = "https://www.dr.com.tr/yayinevi/dogan-kitap/s=2744"
-kronik_url = "https://www.dr.com.tr/yayinevi/kronik-kitap/s=5786"
-sel_url = "https://www.dr.com.tr/yayinevi/sel-yayincilik/s=8753"
-altin_url = "https://www.dr.com.tr/yayinevi/altin-kitaplar/s=510"
-kodlab_url = "https://www.dr.com.tr/yayinevi/kodlab/s=5693"
+ithaki_dr_url = "https://www.dr.com.tr/Yayinevi/ithaki-yayinlari/s=5119"
+isbankasi_dr_url = "https://www.dr.com.tr/Yayinevi/is-bankasi-kultur-yayinlari/s=5109"
+yky_dr_url = "https://www.dr.com.tr/Yayinevi/yapi-kredi-yayinlari/s=10615"
+can_dr_url = "https://www.dr.com.tr/Yayinevi/can-yayinlari/s=1883"
+dogan_dr_url = "https://www.dr.com.tr/yayinevi/dogan-kitap/s=2744"
+kronik_dr_url = "https://www.dr.com.tr/yayinevi/kronik-kitap/s=5786"
+sel_dr_url = "https://www.dr.com.tr/yayinevi/sel-yayincilik/s=8753"
+altin_dr_url = "https://www.dr.com.tr/yayinevi/altin-kitaplar/s=510"
+ayrinti_dr_url = "https://www.dr.com.tr/yayinevi/ayrinti-yayinlari/s=1113"
+kodlab_dr_url = "https://www.dr.com.tr/yayinevi/kodlab/s=5693"
 
-ithaki_html = requests.get(ithaki_url).content
-isbankasi_html = requests.get(isbankasi_url).content
-yky_html = requests.get(yky_url).content
-can_html = requests.get(can_url).content
-dogan_html = requests.get(dogan_url).content
-kronik_html = requests.get(kronik_url).content
-sel_html = requests.get(sel_url).content
-altin_html = requests.get(altin_url).content
-kodlab_html = requests.get(kodlab_url).content
 
-ithaki_soup = BeautifulSoup(ithaki_html, "html.parser")
-isbankasi_soup = BeautifulSoup(isbankasi_html, "html.parser")
-yky_soup = BeautifulSoup(yky_html, "html.parser")
-can_soup = BeautifulSoup(can_html, "html.parser")
-dogan_soup = BeautifulSoup(dogan_html, "html.parser")
-kronik_soup = BeautifulSoup(kronik_html, "html.parser")
-sel_soup = BeautifulSoup(sel_html, "html.parser")
-altin_soup = BeautifulSoup(altin_html, "html.parser")
-kodlab_soup = BeautifulSoup(kodlab_html, "html.parser")
+ithaki_idefix_url = "https://www.idefix.com/yayinevi/ithaki-yayinlari/s=5119"
+isbankasi_idefix_url = "https://www.idefix.com/yayinevi/is-bankasi-kultur-yayinlari/s=5109"
+yky_idefix_url = "https://www.idefix.com/yayinevi/yapi-kredi-yayinlari/s=10615"
+can_idefix_url = "https://www.idefix.com/yayinevi/can-yayinlari/s=1883"
+dogan_idefix_url = "https://www.idefix.com/yayinevi/dogan-kitap/s=2744"
+kronik_idefix_url = "https://www.idefix.com/yayinevi/kronik-kitap/s=5786"
+sel_idefix_url = "https://www.idefix.com/yayinevi/sel-yayincilik/s=8753"
+altin_idefix_url = "https://www.idefix.com/yayinevi/altin-kitaplar/s=510"
+ayrinti_idefix_url = "https://www.idefix.com/yayinevi/ayrinti-yayinlari/s=1113"
+kodlab_idefix_url = "https://www.idefix.com/yayinevi/kodlab/s=5693"
 
-def ithaki():
-    list = ithaki_soup.find("div",{"id":"container"}).find_all("div",{"class":"prd-content"})
-    count = 0
-    titleScript(ithaki_soup)
 
-    for i in list:
-        name = i.find("a", {"class":"prd-name"}).string
-        author = i.find("a", {"class":"who"})
-        price = i.find("div", {"class":"prd-price"}).string
-        count +=1
+def titleScript(param, store):
+    if store == "dr":
+        title = param.find("h1",{"class":"cat-name"}).string
+        print("-"*120)
+        print("|" + title.rjust(60) + "|".rjust(59," "))
+        print("-"*120)
+    elif store == "idefix":
+        title = param.find("h1").string
+        #print("BURADAYIM")
+        print("-"*120)
+        print("|" + title.rjust(60) + "|".rjust(59," "))
+        print("-"*120)
 
-        if not author:
-            author = "Anonim"
-        else:
-            author = author.string
+def authorCheck(author):
+    if not author:
+        author = "Anonim"
+    else:
+        author = author.string
 
-        print(str(count).rjust(2), name.ljust(70,"."), author.ljust(30), price.strip().ljust(5))
+    return author
 
-def isbankasi():
-    list = isbankasi_soup.find("div",{"id":"container"}).find_all("div",{"class":"prd-content"})
-    count = 0
-    titleScript(isbankasi_soup)
+def getSoup(url, store):
+    html_request = requests.get(url).content
+    soup = BeautifulSoup(html_request, "html.parser")
+    getList(soup, store)
 
-    for i in list:
-        name = i.find("a", {"class":"prd-name"}).string
-        author = i.find("a", {"class":"who"})
-        price = i.find("div", {"class":"prd-price"}).string
-        count +=1
+# TODO getList fonksiyonu sadece D&R çin ayarlı onu düzelt
+def getList(soup, store):
+    if store == "dr":
+        list = soup.find("div",{"id":"container"}).find_all("div",{"class":"prd-content"})
+        count = 0
+        titleScript(soup, store)
 
-        if not author:
-            author = "Anonim"
-        else:
-            author = author.string
+        for i in list:
+            name = i.find("a", {"class":"prd-name"}).string
+            author = i.find("a", {"class":"who"})
+            price = i.find("div", {"class":"prd-price"}).string
+            count +=1
 
-        print(str(count).rjust(2), name.ljust(70,"."), author.ljust(30), price.strip().ljust(5))
+            author = authorCheck(author)
 
-def yky():
-    list = yky_soup.find("div",{"id":"container"}).find_all("div",{"class":"prd-content"})
-    count = 0
-    titleScript(yky_soup)
+            print(str(count).rjust(2), name.ljust(70,"."), author.ljust(30), price.strip().ljust(5))
 
-    for i in list:
-        name = i.find("a", {"class":"prd-name"}).string
-        author = i.find("a", {"class":"who"})
-        price = i.find("div", {"class":"prd-price"}).string
-        count +=1
+    elif store == "idefix":
+        list = soup.find("div",{"id":"container"}).find_all("div",{"class":"prd-content"})
+        count = 0
+        titleScript(soup, store)
 
-        if not author:
-            author = "Anonim"
-        else:
-            author = author.string
+    
 
-        print(str(count).rjust(2), name.ljust(70,"."), author.ljust(30), price.strip().ljust(5))
+def ithaki(store):
+    getSoup(ithaki_dr_url, store)
 
-def can():
-    list = can_soup.find("div",{"id":"container"}).find_all("div",{"class":"prd-content"})
-    count = 0
-    titleScript(can_soup)
+def isbankasi(store):
+    getSoup(isbankasi_dr_url, store)
 
-    for i in list:
-        name = i.find("a", {"class":"prd-name"}).string
-        author = i.find("a", {"class":"who"})
-        price = i.find("div", {"class":"prd-price"}).string
-        count +=1
+def yky(store):
+    getSoup(yky_dr_url, store)
 
-        if not author:
-            author = "Anonim"
-        else:
-            author = author.string
+def can(store):
+    getSoup(can_dr_url, store)
 
-        print(str(count).rjust(2), name.ljust(70,"."), author.ljust(30), price.strip().ljust(5))
+def dogan(store):
+    getSoup(dogan_dr_url, store)
 
-def dogan():
-    list = dogan_soup.find("div",{"id":"container"}).find_all("div",{"class":"prd-content"})
-    count = 0
-    titleScript(dogan_soup)
+def kronik(store):
+    getSoup(kronik_dr_url, store)
 
-    for i in list:
-        name = i.find("a", {"class":"prd-name"}).string
-        author = i.find("a", {"class":"who"})
-        price = i.find("div", {"class":"prd-price"}).string
-        count +=1
+def sel(store):
+    getSoup(sel_dr_url, store)
 
-        if not author:
-            author = "Anonim"
-        else:
-            author = author.string
+def altin(store):
+    getSoup(altin_dr_url, store)
 
-        print(str(count).rjust(2), name.ljust(70,"."), author.ljust(30), price.strip().ljust(5))
+def ayrinti(store):
+    getSoup(ayrinti_dr_url, store)
 
-def kronik():
-    list = kronik_soup.find("div",{"id":"container"}).find_all("div",{"class":"prd-content"})
-    count = 0
-    titleScript(kronik_soup)
+def kodlab(store):
+    getSoup(kodlab_dr_url, store)
 
-    for i in list:
-        name = i.find("a", {"class":"prd-name"}).string
-        author = i.find("a", {"class":"who"})
-        price = i.find("div", {"class":"prd-price"}).string
-        count +=1
-
-        if not author:
-            author = "Anonim"
-        else:
-            author = author.string
-
-        print(str(count).rjust(2), name.ljust(70,"."), author.ljust(30), price.strip().ljust(5))
-
-def sel():
-    list = sel_soup.find("div",{"id":"container"}).find_all("div",{"class":"prd-content"})
-    count = 0
-    titleScript(sel_soup)
-
-    for i in list:
-        name = i.find("a", {"class":"prd-name"}).string
-        author = i.find("a", {"class":"who"})
-        price = i.find("div", {"class":"prd-price"}).string
-        count +=1
-
-        if not author:
-            author = "Anonim"
-        else:
-            author = author.string
-
-        print(str(count).rjust(2), name.ljust(70,"."), author.ljust(30), price.strip().ljust(5))
-
-def altin():
-    list = altin_soup.find("div",{"id":"container"}).find_all("div",{"class":"prd-content"})
-    count = 0
-    titleScript(altin_soup)
-
-    for i in list:
-        name = i.find("a", {"class":"prd-name"}).string
-        author = i.find("a", {"class":"who"})
-        price = i.find("div", {"class":"prd-price"}).string
-        count +=1
-
-        if not author:
-            author = "Anonim"
-        else:
-            author = author.string
-
-        print(str(count).rjust(2), name.ljust(70,"."), author.ljust(30), price.strip().ljust(5))
-
-def kodlab():
-    list = kodlab_soup.find("div",{"id":"container"}).find_all("div",{"class":"prd-content"})
-    count = 0
-    titleScript(kodlab_soup)
-
-    for i in list:
-        name = i.find("a", {"class":"prd-name"}).string
-        author = i.find("a", {"class":"who"})
-        price = i.find("div", {"class":"prd-price"}).string
-        count +=1
-
-        if not author:
-            author = "Anonim"
-        else:
-            author = author.string
-
-        print(str(count).rjust(2), name.ljust(70,"."), author.ljust(30), price.strip().ljust(5))
-
+#! Burada klavyeden farklı bir şey girerse sorgusu yok!!!
 def menu():
     while True:
         print("-"*30)
-        choice = input("1- Ithaki Yayinlari\n2- Is Bankasi Yayinlari\n3- Yapı Kredi Yayinlari\n4- Can Yayinlari\n5- Dogan Kitap\n6- Kronik Kitap\n7- Sel Yayincilik\n8- Altin Kitap\n9- Kodlab\n0- Exit\nYour Choice?: ")
+        store_choice = input("1- D&R\n2- Idefix \n0- Exit\nYour Choice?: ")
+        print("-"*30)
+        if store_choice == "0":
+            break
+        else:
+            if store_choice == "1":
+                store = "dr"
+            elif store_choice == "2":
+                store = "idefix"
+
+        print("-"*30)
+        choice = input("1- Ithaki Yayinlari\n2- Is Bankasi Yayinlari\n3- Yapı Kredi Yayinlari\n4- Can Yayinlari\n5- Dogan Kitap\n6- Kronik Kitap\n7- Sel Yayincilik\n8- Altin Kitap\n9- Ayrinti Yayinlari\n10- Kodlab\n0- Back\nYour Choice?: ")
         print("-"*30)
         if choice == "0":
             break
         else:
             if choice=="1":
-                ithaki()
+                ithaki(store)
             elif choice=="2":
-                isbankasi()
+                isbankasi(store)
             elif choice=="3":
-                yky()
+                yky(store)
             elif choice=="4":
-                can()
+                can(store)
             elif choice=="5":
-                dogan()
+                dogan(store)
             elif choice=="6":
-                kronik()
+                kronik(store)
             elif choice=="7":
-                sel()
+                sel(store)
             elif choice=="8":
-                altin()
+                altin(store)
             elif choice=="9":
-                kodlab()
+                ayrinti(store)
+            elif choice=="10":
+                kodlab(store)
 
-def titleScript(param):
-    title = param.find("h1",{"class":"cat-name"}).string
-    print("-"*120)
-    print("|" + title.rjust(60) + "|".rjust(59," "))
-    print("-"*120)
 
 menu()
 
